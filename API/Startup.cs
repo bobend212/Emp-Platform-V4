3fe4a898ca19f838013fb7156801db05036ba751
Mateusz Konopka <mkonopkadev@gmail.com>
2021-03-21 21:38:31 +0100

    CORS added

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,6 +31,7 @@ namespace API
             {
                 options.UseSqlite(_config.GetConnectionString("Default"));
             });
+            services.AddCors();
 
 
             services.AddControllers();
@@ -53,6 +54,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
             app.UseAuthorization();
 
--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Client';
+export class AppComponent implements OnInit {
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/users').subscribe(users => {
+      this.users = users;
+    }, error => {
+      console.log(error);
+    })
+  }
+
+
 }
