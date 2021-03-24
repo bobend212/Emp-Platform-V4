56ba14bbdf27a9d0df5a74a4eaa44830bfd0be9c
Mateusz Konopka <mkonopkadev@gmail.com>
2021-03-24 09:11:08 +0100

    Homepage template added

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { RegisterComponent } from './register/register.component';
 import { UnauthComponent } from './unauth/unauth.component';
@@ -10,6 +11,7 @@ const routes: Routes = [
   {path: 'register', component: RegisterComponent},
   {path: 'unauth', component: UnauthComponent},
   {path: 'projects', component: ProjectsComponent, canActivate: [AuthGuard]},
+  {path: '**', component: HomeComponent}
 ];
 
 @NgModule({
